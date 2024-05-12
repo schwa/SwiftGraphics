655d6416d4f476c7b563ff0187ecdb43747ad23e
Jonathan Wight <jwight@mac.com>
2024-05-12 13:13:27 -0700

    WIP

--- Examples/SwiftGraphicsDemos/Packages/Compute/Package.swift
+++ Examples/SwiftGraphicsDemos/Packages/Compute/Package.swift
@@ -16,14 +16,13 @@ let package = Package(
             targets: ["Compute"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/schwa/SwiftGraphics", branch: "jwight/develop"),
         .package(path: "../../.."),
     ],
     targets: [
         .target(name: "Compute", dependencies: [
             .product(name: "MetalSupport", package: "SwiftGraphics"),
             .product(name: "CoreGraphicsSupport", package: "SwiftGraphics"),
-            .product(name: "RenderKit", package: "RenderKit"),
+            .product(name: "RenderKit", package: "SwiftGraphics"),
         ]),
         .executableTarget(name: "ComputeTool", dependencies: ["Compute"], resources: [
             .process("BitonicSort.metal"),
