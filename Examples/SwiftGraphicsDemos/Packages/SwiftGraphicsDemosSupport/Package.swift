68661035d27952f534d89cab09dbbea70cd1adb3
Jonathan Wight <jwight@mac.com>
2024-05-13 20:27:47 -0700

    WIP

--- Examples/SwiftGraphicsDemos/Packages/SwiftGraphicsDemosSupport/Package.swift
+++ Examples/SwiftGraphicsDemos/Packages/SwiftGraphicsDemosSupport/Package.swift
@@ -1,32 +1,23 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "DemosSupport",
-    platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
-        .macCatalyst(.v17),
-        .tvOS(.v17),
-        .visionOS(.v1),
-    ],
+    name: "SwiftGraphicsDemosSupport",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "DemosSupport",
-            targets: ["DemosSupport"]
-        ),
+            name: "SwiftGraphicsDemosSupport",
+            targets: ["SwiftGraphicsDemosSupport"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DemosSupport"),
+            name: "SwiftGraphicsDemosSupport"),
         .testTarget(
-            name: "DemosSupportTests",
-            dependencies: ["DemosSupport"]
-        ),
+            name: "SwiftGraphicsDemosSupportTests",
+            dependencies: ["SwiftGraphicsDemosSupport"]),
     ]
 )
