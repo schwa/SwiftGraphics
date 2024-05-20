4e0aff41bf7ac0badfb70f7eca6f980c1772e254
Jonathan Wight <jwight@mac.com>
2024-05-19 22:21:12 -0700

    tidy up

--- Examples/SwiftGraphicsDemos/Packages/SwiftGraphicsDemosSupport/Sources/SwiftGraphicsDemosSupport/Support/MeshRepresentation/HalfEdge.swift
+++ Examples/SwiftGraphicsDemos/Packages/SwiftGraphicsDemosSupport/Sources/SwiftGraphicsDemosSupport/Support/MeshRepresentation/HalfEdge.swift
@@ -21,11 +21,23 @@ public struct HalfEdgeMesh {
         }
     }
 
+    public class Edge: Identifiable {
+        var halfEdge: HalfEdge?
+
+        var twin: HalfEdge? {
+            return halfEdge?.twin
+        }
+
+        init() {
+        }
+    }
+
     public class HalfEdge: Identifiable {
         var vertex: Vertex
         var next: HalfEdge?
         var twin: HalfEdge?
         var face: Face?
+        var edge: Edge?
 
         init(vertex: Vertex, nextEdge: HalfEdge?, twinEdge: HalfEdge?, face: Face?) {
             self.vertex = vertex
@@ -41,6 +53,8 @@ public struct HalfEdgeMesh {
 
 extension HalfEdgeMesh {
     mutating func addFace(positions: [SIMD3<Float>]) {
+        // TODO: Twin is not being added.
+        // TODO: Edge is not being added
         let face = Face()
         let vertices = positions.map { Vertex(position: $0) }
         let halfEdges = vertices.map { HalfEdge(vertex: $0, nextEdge: nil, twinEdge: nil, face: face) }
@@ -69,56 +83,5 @@ extension HalfEdgeMesh.Face {
 
     var vertices: [HalfEdgeMesh.Vertex] {
         halfEdges.map(\.vertex)
-    }
-}
-
-// MARK: -
-
-extension HalfEdgeMesh {
-    var polygons: [Shapes3D.Polygon3D<SIMD3<Float>>] {
-        faces.map(\.polygon)
-    }
-}
-
-extension HalfEdgeMesh.Face {
-    var polygon: Polygon3D<SIMD3<Float>> {
-        .init(vertices: vertices.map(\.position))
-    }
-}
-
-// MARK: -
-
-public protocol HalfEdgeMeshConverterProtocol: ConverterProtocol {
-
-}
-
-public protocol HalfEdgeMeshConvertable {
-    associatedtype HalfEdgeMeshConverter: HalfEdgeMeshConverterProtocol where HalfEdgeMeshConverter.Input == Self, HalfEdgeMeshConverter.Output == HalfEdgeMesh
-
-    func toHalfEdgeMesh() throws -> HalfEdgeMesh
-}
-
-extension Box3D: HalfEdgeMeshConvertable {
-    public struct HalfEdgeMeshConverter: HalfEdgeMeshConverterProtocol {
-        public func convert(_ box: Box3D) throws -> HalfEdgeMesh {
-            var mesh = HalfEdgeMesh()
-            // Bottom face (viewed from above, must be clockwise because normally viewed from below)
-            mesh.addFace(positions: [box.minXMinYMinZ, box.maxXMinYMinZ, box.maxXMaxYMinZ, box.minXMaxYMinZ])
-            // Top face (viewed from above)
-            mesh.addFace(positions: [box.minXMinYMaxZ, box.minXMaxYMaxZ, box.maxXMaxYMaxZ, box.maxXMinYMaxZ])
-            // Left face (viewed from left)
-            mesh.addFace(positions: [box.minXMinYMaxZ, box.minXMinYMinZ, box.minXMaxYMinZ, box.minXMaxYMaxZ])
-            // Right face (viewed from right)
-            mesh.addFace(positions: [box.maxXMinYMinZ, box.maxXMinYMaxZ, box.maxXMaxYMaxZ, box.maxXMaxYMinZ])
-            // Front face (viewed from front)
-            mesh.addFace(positions: [box.minXMinYMinZ, box.minXMinYMaxZ, box.maxXMinYMaxZ, box.maxXMinYMinZ])
-            // Back face (viewed from back)
-            mesh.addFace(positions: [box.minXMaxYMinZ, box.maxXMaxYMinZ, box.maxXMaxYMaxZ, box.minXMaxYMaxZ])
-            return mesh
-        }
-    }
-
-    public func toHalfEdgeMesh() throws -> HalfEdgeMesh {
-        try HalfEdgeMeshConverter().convert(self)
     }
 }
