bb380077b492385a06f67d0a336a977a06082fd6
Jonathan Wight <schwa@schwa.io>
2024-06-20 13:19:29 -0700

    Make splat rate adjustable. Load splatc files from disk.

--- Sources/SwiftGraphicsDemosSupport/Demos/GaussianSplats/GaussianSplatView.swift
+++ Sources/SwiftGraphicsDemosSupport/Demos/GaussianSplats/GaussianSplatView.swift
@@ -7,28 +7,75 @@ import MetalSupport
 import Shapes3D
 import MetalKit
 import simd
+import Observation
+import Everything
+import UniformTypeIdentifiers
 
-//struct SplatC {
-//    var position: SIMD3<Float16>
-//    var color: SIMD4<Float16>
-//    var cov_a: SIMD3<Float16>
-//    var cov_b: SIMD3<Float16>
-//};
-
+extension UTType {
+    static let splatC = UTType(filenameExtension: "splatc")!
+}
 
 struct GaussianSplatView: View, DemoView {
+
     @State
+    var device = MTLCreateSystemDefaultDevice()!
+
+    @State
+    var viewModel: GaussianSplatViewModel?
+
+    var body: some View {
+        ZStack {
+            Color.white
+            if let viewModel {
+                GaussianSplatRenderView(device: device).environment(viewModel)
+            }
+        }
+        .toolbar {
+            ValueView(value: false) { isPresented in
+                Toggle("Load", isOn: isPresented)
+                .fileImporter(isPresented: isPresented, allowedContentTypes: [.splatC]) { result in
+                    if case let .success(url) = result {
+                        viewModel = try! GaussianSplatViewModel(device: device, url: url)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            let url = Bundle.module.url(forResource: "train.splatc", withExtension: "data")!
+            viewModel = try! GaussianSplatViewModel(device: device, url: url)
+        }
+
+    }
+}
+
+@Observable
+class GaussianSplatViewModel {
     var splatCount: Int
-
-    @State
     var splats: MTLBuffer
-
-    @State
     var splatIndices: MTLBuffer
-
-    @State
     var splatDistances: MTLBuffer
+    var cube: MTKMesh // TODO: RENAME
 
+    init(device: MTLDevice, url: URL) throws {
+        let data = try! Data(contentsOf: url)
+        let splatSize = 26
+        let splatCount = data.count / splatSize
+        splats = device.makeBuffer(data: data, options: .storageModeShared)!.labelled("Splats")
+        let splatIndicesData = (0 ..< splatCount).map { UInt32($0) }.withUnsafeBytes {
+            Data($0)
+        }
+        splatIndices = device.makeBuffer(data: splatIndicesData, options: .storageModeShared)!.labelled("Splats-Indices")
+        splatDistances = device.makeBuffer(length: MemoryLayout<Float>.size * splatCount, options: .storageModeShared)!.labelled("Splat-Distances")
+        self.splatCount = splatCount
+
+        let allocator = MTKMeshBufferAllocator(device: device)
+        cube = try! MTKMesh(mesh: MDLMesh(planeWithExtent: [2, 2, 0], segments: [1, 1], geometryType: .triangles, allocator: allocator), device: device)
+    }
+
+}
+
+
+struct GaussianSplatRenderView: View {
     @State
     var cameraTransform: Transform = .translation([0, 0, 3])
 
@@ -42,52 +89,29 @@ struct GaussianSplatView: View, DemoView {
     var device: MTLDevice
 
     @State
-    var cube: MTKMesh
-
-    @State
     var debugMode: Bool = false
 
-    init() {
-        let device = MTLCreateSystemDefaultDevice()!
-        let url = Bundle.module.url(forResource: "train.splatc", withExtension: "data")!
-        var data = try! Data(contentsOf: url)
-//        print(Float(data.count) / 26)
-        let splatSize = 26
-        let splatCount = data.count / splatSize
-        splats = device.makeBuffer(data: data, options: .storageModeShared)!.labelled("Splats")
+    @State
+    var sortRate: Int = 1
 
-        let splatIndicesData = (0 ..< splatCount).shuffled().map { UInt32($0) }.withUnsafeBytes {
-            Data($0)
-        }
-        splatIndices = device.makeBuffer(data: splatIndicesData, options: .storageModeShared)!.labelled("Splats-Indices")
-//        print("#splats", splatCount);
-
-        splatDistances = device.makeBuffer(length: MemoryLayout<Float>.size * splatCount, options: .storageModeShared)!.labelled("Splat-Distances")
-
-        self.device = device
-        self.splatCount = splatCount
-
-        let size: Float = 0.005
-//        cube = try! Box3D(min: [-size, -size, -size], max: [size, size, size]).toMTKMesh(device: device)
-
-        let allocator = MTKMeshBufferAllocator(device: device)
-        cube = try! MTKMesh(mesh: MDLMesh(planeWithExtent: [2, 2, 0], segments: [1, 1], geometryType: .triangles, allocator: allocator), device: device)
-
-        //{ { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
-
-
-//        debugSort()
-    }
+    @Environment(GaussianSplatViewModel.self)
+    var viewModel
 
     var body: some View {
         RenderView(device: device, passes: passes)
         .ballRotation($modelTransform.rotation.rollPitchYaw, pitchLimit: .radians(-.infinity) ... .radians(.infinity))
         .overlay(alignment: .bottom) {
             VStack {
-                Text("#splats: \(splatCount)")
+                Text("#splats: \(viewModel.splatCount)")
                 Slider(value: $cameraTransform.translation.z, in: 0.0 ... 20.0) { Text("Distance") }
                     .frame(maxWidth: 120)
                 Toggle("Debug Mode", isOn: $debugMode)
+                HStack {
+                    Slider(value: $sortRate.toDouble, in: 1 ... 60) { Text("Sort Rate") }
+                    .frame(maxWidth: 120)
+                    Text("\(sortRate)")
+                }
+
             }
             .padding()
             .background(.ultraThickMaterial).cornerRadius(8)
@@ -97,28 +121,29 @@ struct GaussianSplatView: View, DemoView {
 
     var passes: [any PassProtocol] {
         let preCalcComputePass = GaussianSplatPreCalcComputePass(
-            splatCount: splatCount,
-            splatDistancesBuffer: Box(splatDistances),
-            splatBuffer: Box(splats),
+            splatCount: viewModel.splatCount,
+            splatDistancesBuffer: Box(viewModel.splatDistances),
+            splatBuffer: Box(viewModel.splats),
             modelMatrix: simd_float3x3(truncating: modelTransform.matrix),
             cameraPosition: cameraTransform.translation
         )
 
         let gaussianSplatSortComputePass = GaussianSplatBitonicSortComputePass(
-            splatCount: splatCount,
-            splatIndicesBuffer: Box(splatIndices),
-            splatDistancesBuffer: Box(splatDistances)
+            splatCount: viewModel.splatCount,
+            splatIndicesBuffer: Box(viewModel.splatIndices),
+            splatDistancesBuffer: Box(viewModel.splatDistances),
+            sortRate: sortRate
         )
 
         let gaussianSplatRenderPass = GaussianSplatRenderPass(
             cameraTransform: cameraTransform,
             cameraProjection: cameraProjection,
             modelTransform: modelTransform,
-            splatCount: splatCount,
-            splats: Box(splats),
-            splatIndices: Box(splatIndices),
-            splatDistances: Box(splatDistances),
-            pointMesh: cube,
+            splatCount: viewModel.splatCount,
+            splats: Box(viewModel.splats),
+            splatIndices: Box(viewModel.splatIndices),
+            splatDistances: Box(viewModel.splatDistances),
+            pointMesh: viewModel.cube,
             debugMode: debugMode
         )
 
@@ -242,4 +267,15 @@ struct GaussianSplatRenderPass: RenderPassProtocol {
 
     }
 
+}
+
+extension Int {
+var toDouble: Double {
+    get {
+        Double(self)
+    }
+    set {
+        self = Int(newValue)
+    }
+}
 }
