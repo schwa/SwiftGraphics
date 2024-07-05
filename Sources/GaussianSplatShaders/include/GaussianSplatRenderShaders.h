856ba5e5f9ae4b1ecc347da4cc1fbeedac170af3
Jonathan Wight <schwa@schwa.io>
2024-07-05 13:41:44 -0700

    Clean up GaussianSplat shader headers.

---
+++ Sources/GaussianSplatShaders/include/GaussianSplatRenderShaders.h
@@ -0,0 +1,92 @@
+#import <simd/simd.h>
+
+#import "GaussianSplatSupport.h"
+
+#ifdef __METAL_VERSION__
+#import <metal_stdlib>
+
+namespace GaussianSplatShaders {
+
+    using namespace metal;
+
+    struct VertexIn {
+        float3 position  [[attribute(0)]];
+    };
+
+    struct VertexOut {
+        float4 position [[position]];
+        float2 relativePosition; // Ranges from -kBoundsRadius to +kBoundsRadius
+        float4 color;
+    };
+
+    struct FragmentOut {
+        float4 fragColor [[color(0)]];
+    };
+
+    typedef SplatC Splat;
+    typedef GaussianSplatUniforms VertexUniforms;
+    typedef GaussianSplatUniforms FragmentUniforms;
+    typedef VertexOut FragmentIn;
+
+    // MARK: -
+
+    constant static const float kBoundsRadius = 2;
+    constant static const float kBoundsRadiusSquared = kBoundsRadius * kBoundsRadius;
+
+    [[vertex]]
+    VertexOut VertexShader(
+        VertexIn in [[stage_in]],
+        uint instance_id[[instance_id]],
+        uint vertex_id[[vertex_id]],
+        constant VertexUniforms &uniforms [[buffer(1)]],
+        constant Splat *splats [[buffer(2)]],
+        constant uint *splatIndices [[buffer(3)]]
+   ) {
+        VertexOut out;
+
+        const float2 vertexModelSpacePosition = in.position.xy;
+        auto splat = splats[splatIndices[instance_id]];
+        const float4 splatWorldSpacePosition = uniforms.modelViewMatrix * float4(float3(splat.position), 1);
+        const float4 splatClipSpacePosition = uniforms.projectionMatrix * splatWorldSpacePosition;
+
+//        const auto bounds = 1.2 * splatClipSpacePosition.w;
+//        if (splatClipSpacePosition.z < -splatClipSpacePosition.w
+//            || splatClipSpacePosition.x < -bounds
+//            || splatClipSpacePosition.x > bounds
+//            || splatClipSpacePosition.y < -bounds
+//            || splatClipSpacePosition.y > bounds) {
+//            out.position = float4(1, 1, 0, 1);
+//            return out;
+//        }
+
+        // float3 calcCovariance2D(float3 viewPos, packed_half3 cov3Da, packed_half3 cov3Db, float4x4 viewMatrix, float4x4 projectionMatrix, float2 screenSize)
+        const float3 cov2D = calcCovariance2D(splatWorldSpacePosition.xyz, splat.cov_a, splat.cov_b, uniforms.viewMatrix, uniforms.projectionMatrix, uniforms.drawableSize);
+        const Tuple2<float2> axes = decomposeCovariance(cov2D);
+
+        const float2 projectedScreenDelta = (vertexModelSpacePosition.x * axes.v0 + vertexModelSpacePosition.y * axes.v1) * 2 * kBoundsRadius / uniforms.drawableSize;
+        out.position = splatClipSpacePosition + float4(projectedScreenDelta.xy * splatClipSpacePosition.w, 0, 0);
+        out.relativePosition = vertexModelSpacePosition * kBoundsRadius;
+        out.color = float4(splat.color);
+        return out;
+    }
+
+    // MARK: -
+
+    [[fragment]]
+    float4 FragmentShader(
+        FragmentIn in [[stage_in]],
+        constant FragmentUniforms &uniforms [[buffer(0)]],
+        constant Splat *splats [[buffer(1)]],
+        constant uint *splatIndices [[buffer(3)]]
+    ) {
+        const auto relativePosition = in.relativePosition;
+        const auto negativeDistanceSquared = -dot(relativePosition, relativePosition);
+        if (negativeDistanceSquared < -kBoundsRadiusSquared) {
+            discard_fragment();
+        }
+        const auto falloff = exp(negativeDistanceSquared);
+        const auto alpha = in.color.a * falloff;
+        return float4(in.color.rgb * alpha, alpha);
+    }
+}
+#endif // __METAL_VERSION__
