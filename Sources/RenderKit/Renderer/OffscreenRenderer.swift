e68ff593e5511c3f09576f7a5a477e3a4ffe3906
Jonathan Wight <jwight@mac.com>
2024-07-11 22:58:32 -0700

    MetalFX experiment.

--- Sources/RenderKit/Renderer/OffscreenRenderer.swift
+++ Sources/RenderKit/Renderer/OffscreenRenderer.swift
@@ -7,23 +7,19 @@ public struct OffscreenRenderer {
     var device: MTLDevice
     var size: CGSize
     var offscreenConfiguration: OffscreenRenderPassConfiguration
+    var renderPassDescriptor: MTLRenderPassDescriptor
     var renderer: Renderer<OffscreenRenderPassConfiguration>
 
     public var targetTexture: MTLTexture? {
-        offscreenConfiguration.targetTexture
+        fatalError()
     }
 
-    public init(device: MTLDevice, size: CGSize, passes: [any PassProtocol]) throws {
+    public init(device: MTLDevice, size: CGSize, offscreenConfiguration: OffscreenRenderPassConfiguration, renderPassDescriptor: MTLRenderPassDescriptor, passes: [any PassProtocol]) throws {
         self.device = device
         self.size = size
-        offscreenConfiguration = OffscreenRenderPassConfiguration(device: device, size: size)
-        offscreenConfiguration.clearColor = .init(red: 0, green: 0, blue: 0, alpha: 0)
-        offscreenConfiguration.depthStencilPixelFormat = .depth32Float
-        offscreenConfiguration.depthStencilStorageMode = .memoryless
-        offscreenConfiguration.clearDepth = 1
-        offscreenConfiguration.colorPixelFormat = .bgra8Unorm_srgb
-        try offscreenConfiguration.update()
-        renderer = Renderer<OffscreenRenderPassConfiguration>(device: device, passes: .init(passes))
+        self.offscreenConfiguration = offscreenConfiguration
+        self.renderPassDescriptor = renderPassDescriptor
+        self.renderer = .init(device: device, passes: .init(passes))
     }
 
     public mutating func configure() throws {
@@ -35,9 +31,30 @@ public struct OffscreenRenderer {
         try device.capture(enabled: false) {
             let commandQueue = try device.makeCommandQueue().safelyUnwrap(MetalSupportError.resourceCreationFailure)
             try commandQueue.withCommandBuffer(waitAfterCommit: true) { commandBuffer in
-                let renderPassDescriptor = try offscreenConfiguration.currentRenderPassDescriptor.safelyUnwrap(MetalSupportError.resourceCreationFailure)
                 try renderer.render(commandBuffer: commandBuffer, renderPassDescriptor: renderPassDescriptor, drawableSize: size)
             }
         }
     }
+}
+
+// MARK: -
+
+public struct OffscreenRenderPassConfiguration: MetalConfigurationProtocol {
+    public var colorPixelFormat: MTLPixelFormat = .bgra8Unorm
+    public var clearColor: MTLClearColor = .init(red: 0, green: 0, blue: 0, alpha: 1.0)
+    public var depthStencilPixelFormat: MTLPixelFormat = .invalid // TODO: NOT USED
+    public var depthStencilStorageMode: MTLStorageMode = .shared // TODO: NOT USED
+    public var clearDepth: Double = 1.0
+
+    public init() {
+
+    }
+
+//    public var currentRenderPassDescriptor: MTLRenderPassDescriptor?
+////    public var targetTexture: MTLTexture? // TODO: Rename - this is too vague
+//
+//    public mutating func update() throws {
+//        currentRenderPassDescriptor = nil
+//        targetTexture = nil
+//    }
 }
