c119dce2cdd0fc1dbdebcb029cef770961c310da
Jonathan Wight <schwa@schwa.io>
2024-07-24 10:32:18 -0700

    Reorganise demos

---
+++ Sources/ComputeDemos/ComputeDemo.swift
@@ -0,0 +1,10 @@
+@main
+enum Main {
+    static func main() throws {
+        // try BitonicSortDemo().main()
+        // try GameOfLife().main()
+        // try RandomFill().main()
+        // try Reduce().main()
+        try RadixSort().main()
+    }
+}
