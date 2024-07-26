dd0c84501731aca376d4026ab2f8a1de1a6b5e32
Jonathan Wight <jwight@mac.com>
2024-07-26 14:07:28 -0700

    Hmm PackedFloat4. WOT?

--- Tests/SIMDSupportTests/PackedFloatTests.swift
+++ Tests/SIMDSupportTests/PackedFloatTests.swift
@@ -15,4 +15,9 @@ func testPackedFloatSizes() {
     #expect(MemoryLayout<SIMD3<Float>>.stride == 16)
     #expect(MemoryLayout<PackedFloat3>.size == 12)
     #expect(MemoryLayout<PackedFloat3>.stride == 12)
+
+    #expect(MemoryLayout<PackedHalf4>.size == 8)
+    #expect(MemoryLayout<PackedHalf4>.stride == 8)
+    #expect(MemoryLayout<simd_packed_half4>.size == 8)
+    #expect(MemoryLayout<simd_packed_half4>.stride == 8)
 }
