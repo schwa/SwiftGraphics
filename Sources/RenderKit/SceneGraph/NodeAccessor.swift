caab5794487dd1a1443f62ae0232ff7e272be934
Jonathan Wight <jwight@mac.com>
2024-07-26 14:05:48 -0700

    Start cleaning up NodeAccessor.

--- Sources/RenderKit/SceneGraph/NodeAccessor.swift
+++ Sources/RenderKit/SceneGraph/NodeAccessor.swift
@@ -8,111 +8,45 @@ import SwiftUI
 // We also need ways to query the graph = queries should (generally?) return accessors
 // Query by content type woudl be nice
 
-public struct NodeAccessor: Hashable {
+public struct NodeAccessor: Hashable, Sendable {
     var path: IndexPath
-}
 
-// TODO: Cleanup.
-public extension SceneGraph {
-    // @available(*, deprecated, message: "Deprecated")
-    func firstIndexPath(matching predicate: (Node, IndexPath) -> Bool) -> IndexPath? {
-        root.allIndexedNodes().first { node, indexPath in
-            predicate(node, indexPath)
-        }?.1
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    func firstIndexPath(id: Node.ID) -> IndexPath? {
-        firstIndexPath { node, _ in
-            node.id == id
-        }
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    func firstIndexPath(label: String) -> IndexPath? {
-        firstIndexPath { node, _ in
-            node.label == label
-        }
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    func node(for label: String) -> Node? {
-        root.allIndexedNodes().first { $0.node.label == label }?.node
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    func accessor(for label: String) -> NodeAccessor? {
-        guard let path = root.allIndexedNodes().first(where: { $0.node.label == label })?.path else {
-            return nil
-        }
-        return .init(path: path)
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    func accessor(for id: Node.ID) -> NodeAccessor? {
-        guard let path = root.allIndexedNodes().first(where: { $0.node.id == id })?.path else {
-            return nil
-        }
-        return .init(path: path)
-    }
-
-    subscript(accessor accessor: NodeAccessor) -> Node? {
-        get {
-            root[indexPath: accessor.path]
-        }
-        set {
-            guard let newValue else {
-                // TODO: There's no reason we can't delete the node here.
-                fatalError("Cannot set node to nil")
-            }
-            root[indexPath: accessor.path] = newValue
-        }
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    mutating func modify <R>(label: String, _ block: (inout Node?) throws -> R) throws -> R {
-        guard let accessor = accessor(for: label) else {
-            throw BaseError.missingValue
-        }
-        var node = self[accessor: accessor]
-        let result = try block(&node)
-        self[accessor: accessor] = node
-        return result
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    mutating func modify <R>(id: Node.ID, _ block: (inout Node?) throws -> R) throws -> R {
-        guard let accessor = accessor(for: id) else {
-            throw BaseError.missingValue
-        }
-        var node = self[accessor: accessor]
-        let result = try block(&node)
-        self[accessor: accessor] = node
-        return result
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    mutating func modify <R>(node: Node, _ block: (inout Node?) throws -> R) throws -> R {
-        guard let accessor = accessor(for: node.id) else {
-            throw BaseError.missingValue
-        }
-        var node = self[accessor: accessor]
-        let result = try block(&node)
-        self[accessor: accessor] = node
-        return result
-    }
-
-    // @available(*, deprecated, message: "Deprecated")
-    mutating func modify <R>(accessor: NodeAccessor, _ block: (inout Node?) throws -> R) rethrows -> R {
-        var node = self[accessor: accessor]
-        let result = try block(&node)
-        self[accessor: accessor] = node
-        return result
+    init(_ path: IndexPath) {
+        self.path = path
     }
 }
 
 public extension Node {
-    // TODO: Deprecate and use Accessors.
+    subscript(accessor accessor: NodeAccessor) -> Node {
+        get {
+            guard let index = accessor.path.first else {
+                return self
+            }
+            let child = children[index]
+            let indexPath = accessor.path.dropFirst()
+            if indexPath.isEmpty {
+                return child
+            }
+            else {
+                return child[accessor: .init(indexPath)]
+            }
+        }
+        set {
+            guard let index = accessor.path.first else {
+                self = newValue
+                return
+            }
+            let indexPath = accessor.path.dropFirst()
+            if indexPath.isEmpty {
+                children[index] = newValue
+            }
+            else {
+                children[index][accessor: .init(indexPath)] = newValue
+            }
+        }
+    }
+
+    @available(*, deprecated, message: "Deprecated")
     subscript(indexPath indexPath: IndexPath) -> Node {
         get {
             guard let index = indexPath.first else {
@@ -162,37 +96,135 @@ public extension Node {
 
 // MARK: -
 
+public extension SceneGraph {
+    @available(*, deprecated, message: "Deprecated")
+    func firstIndexPath(matching predicate: (Node, IndexPath) -> Bool) -> IndexPath? {
+        root.allIndexedNodes().first { node, indexPath in
+            predicate(node, indexPath)
+        }?.1
+    }
+
+    @available(*, deprecated, message: "Deprecated")
+    func firstIndexPath(id: Node.ID) -> IndexPath? {
+        firstIndexPath { node, _ in
+            node.id == id
+        }
+    }
+
+    @available(*, deprecated, message: "Deprecated")
+    func firstIndexPath(label: String) -> IndexPath? {
+        firstIndexPath { node, _ in
+            node.label == label
+        }
+    }
+
+    @available(*, deprecated, message: "Deprecated")
+    func node(for label: String) -> Node? {
+        root.allIndexedNodes().first { $0.node.label == label }?.node
+    }
+
+    @available(*, deprecated, message: "Deprecated")
+    func accessor(for label: String) -> NodeAccessor? {
+        guard let path = root.allIndexedNodes().first(where: { $0.node.label == label })?.path else {
+            return nil
+        }
+        return .init(path)
+    }
+
+    @available(*, deprecated, message: "Deprecated")
+    func accessor(for id: Node.ID) -> NodeAccessor? {
+        guard let path = root.allIndexedNodes().first(where: { $0.node.id == id })?.path else {
+            return nil
+        }
+        return .init(path)
+    }
+
+    subscript(accessor accessor: NodeAccessor) -> Node? {
+        get {
+            root[accessor: accessor]
+        }
+        set {
+            guard let newValue else {
+                // IDEA: There's no reason we can't delete the node here.
+                fatalError("Cannot set node to nil")
+            }
+            root[accessor: accessor] = newValue
+        }
+    }
+
+    mutating func modify <R>(accessor: NodeAccessor, _ block: (inout Node?) throws -> R) rethrows -> R {
+        var node = self[accessor: accessor]
+        let result = try block(&node)
+        self[accessor: accessor] = node
+        return result
+    }
+
+    mutating func modify <R>(label: String, _ block: (inout Node?) throws -> R) throws -> R {
+        guard let accessor = accessor(for: label) else {
+            throw BaseError.missingValue
+        }
+        var node = self[accessor: accessor]
+        let result = try block(&node)
+        self[accessor: accessor] = node
+        return result
+    }
+
+    mutating func modify <R>(id: Node.ID, _ block: (inout Node?) throws -> R) throws -> R {
+        guard let accessor = accessor(for: id) else {
+            throw BaseError.missingValue
+        }
+        var node = self[accessor: accessor]
+        let result = try block(&node)
+        self[accessor: accessor] = node
+        return result
+    }
+
+    mutating func modify <R>(node: Node, _ block: (inout Node?) throws -> R) throws -> R {
+        guard let accessor = accessor(for: node.id) else {
+            throw BaseError.missingValue
+        }
+        var node = self[accessor: accessor]
+        let result = try block(&node)
+        self[accessor: accessor] = node
+        return result
+    }
+}
+
+
+
+// MARK: -
+
 public extension Binding where Value == SceneGraph {
     func binding(for label: String) -> Binding<Node?> {
         guard let indexPath = wrappedValue.firstIndexPath(label: label) else {
             fatalError("No node with label \(label) found in scene graph.")
         }
         return Binding<Node?> {
-            wrappedValue[accessor: NodeAccessor(path: indexPath)]
+            wrappedValue[accessor: NodeAccessor(indexPath)]
         }
         set: {
-            wrappedValue[accessor: NodeAccessor(path: indexPath)] = $0
+            wrappedValue[accessor: NodeAccessor(indexPath)] = $0
         }
     }
 
     func binding(for indexPath: IndexPath) -> Binding<Node?> {
         Binding<Node?> {
-            wrappedValue[accessor: NodeAccessor(path: indexPath)]
+            wrappedValue[accessor: NodeAccessor(indexPath)]
         }
         set: {
-            wrappedValue[accessor: NodeAccessor(path: indexPath)] = $0
+            wrappedValue[accessor: NodeAccessor(indexPath)] = $0
         }
     }
 
     func binding(for indexPath: IndexPath) -> Binding<Node> {
         Binding<Node> {
-            guard let node = wrappedValue[accessor: NodeAccessor(path: indexPath)] else {
+            guard let node = wrappedValue[accessor: NodeAccessor(indexPath)] else {
                 fatalError("No node at indexPath \(indexPath)")
             }
             return node
         }
         set: {
-            wrappedValue[accessor: NodeAccessor(path: indexPath)] = $0
+            wrappedValue[accessor: NodeAccessor(indexPath)] = $0
         }
     }
 }
