ea2858182769596a3adda068703aa1fc1a27a5b9
Jonathan Wight <jwight@mac.com>
2024-07-31 10:29:45 -0700

    Dependency cleanup.

--- Sources/RenderKitSceneGraph/Support.swift
+++ Sources/RenderKitSceneGraph/Support.swift
@@ -1,5 +1,3 @@
-import RenderKitSceneGraph
-
 public extension SceneGraph {
     static let basicScene = SceneGraph(root: Node(label: "Root", children: [
         Node(label: "camera", content: Camera())
