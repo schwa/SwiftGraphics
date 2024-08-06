618601e767118f20afc62e14113f386f01cd714e
Jonathan Wight <schwa@schwa.io>
2024-08-06 13:17:28 -0700

    WIP

--- Sources/SIMDSupport/RollPitchYaw.swift
+++ Sources/SIMDSupport/RollPitchYaw.swift
@@ -77,6 +77,67 @@ public extension XYZRotation {
 //        )
 //    }
 
+
+public extension XYZRotation {
+    enum Order: CaseIterable {
+        case xyz
+        case xzy
+        case yxz
+        case yzx
+        case zxy
+        case zyx
+    }
+
+    func toMatrix3x3(order: Order) -> simd_float3x3 {
+        let cx = Float(cos(x.angle))
+        let sx = Float(sin(x.angle))
+        let cy = Float(cos(y.angle))
+        let sy = Float(sin(y.angle))
+        let cz = Float(cos(z.angle))
+        let sz = Float(sin(z.angle))
+
+        switch order {
+        case .xyz:
+            return simd_float3x3(
+                simd_float3(cy * cz, cy * sz, -sy),
+                simd_float3(sx * sy * cz - cx * sz, sx * sy * sz + cx * cz, sx * cy),
+                simd_float3(cx * sy * cz + sx * sz, cx * sy * sz - sx * cz, cx * cy)
+            )
+        case .xzy:
+            return simd_float3x3(
+                simd_float3(cy * cz, sz, -sy * cz),
+                simd_float3(-cy * sz * cx + sy * sx, cx * cz, sy * sz * cx + cy * sx),
+                simd_float3(cy * sz * sx + sy * cx, -sx * cz, -sy * sz * sx + cy * cx)
+            )
+        case .yxz:
+            return simd_float3x3(
+                simd_float3(cy * cz + sy * sx * sz, cy * sz - sy * sx * cz, -sy * cx),
+                simd_float3(cx * sz, cx * cz, sx),
+                simd_float3(sy * cz - cy * sx * sz, sy * sz + cy * sx * cz, cy * cx)
+            )
+        case .yzx:
+            return simd_float3x3(
+                simd_float3(cy * cz, sx * sy - cx * cy * sz, cx * sy + sx * cy * sz),
+                simd_float3(sz, cx * cz, -sx * cz),
+                simd_float3(-sy * cz, sx * cy + cx * sy * sz, cx * cy - sx * sy * sz)
+            )
+        case .zxy:
+            return simd_float3x3(
+                simd_float3(cy * cz, sz, -sy * cz),
+                simd_float3(-cx * cy * sz + sx * sy, cx * cz, cx * sy * sz + sx * cy),
+                simd_float3(sx * cy * sz + cx * sy, -sx * cz, -sx * sy * sz + cx * cy)
+            )
+        case .zyx:
+            return simd_float3x3(
+                simd_float3(cy * cz, cx * sz + sx * sy * cz, sx * sz - cx * sy * cz),
+                simd_float3(-cy * sz, cx * cz - sx * sy * sz, sx * cz + cx * sy * sz),
+                simd_float3(sy, -sx * cy, cx * cy)
+            )
+        }
+    }
+}
+
+
 public extension XYZRotation {
     init(target: Target = .object, matrix: simd_float3x3) {
         // https://web.archive.org/web/20220428033032/
