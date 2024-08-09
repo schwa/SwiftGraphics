ad58d48ab7f06ae52b950446f483baca88a089de
Jonathan Wight <schwa@schwa.io>
2024-08-09 13:44:19 -0700

    WIP

--- Sources/SIMDSupport/Axis3D.swift
+++ Sources/SIMDSupport/Axis3D.swift
@@ -17,3 +17,37 @@ public enum Axis3D: CaseIterable {
         }
     }
 }
+
+public extension Axis3D {
+    var color: Color {
+        switch self {
+        case .x:
+            .red
+        case .y:
+            .green
+        case .z:
+            .blue
+        }
+    }
+}
+
+public extension RollPitchYaw {
+    mutating func setAxis(_ vector: SIMD3<Float>) {
+        switch vector {
+        case [-1, 0, 0]:
+            self = .init(roll: .degrees(0), pitch: .degrees(0), yaw: .degrees(90))
+        case [1, 0, 0]:
+            self = .init(roll: .degrees(0), pitch: .degrees(0), yaw: .degrees(270))
+        case [0, -1, 0]:
+            self = .init(roll: .degrees(0), pitch: .degrees(90), yaw: .degrees(0))
+        case [0, 1, 0]:
+            self = .init(roll: .degrees(0), pitch: .degrees(270), yaw: .degrees(0))
+        case [0, 0, -1]:
+            self = .init(roll: .degrees(0), pitch: .degrees(180), yaw: .degrees(0))
+        case [0, 0, 1]:
+            self = .init(roll: .degrees(0), pitch: .degrees(0), yaw: .degrees(0))
+        default:
+            break
+        }
+    }
+}
--- Sources/SwiftGraphicsDemos/Support/Support.swift
+++ Sources/SwiftGraphicsDemos/Support/Support.swift
@@ -706,24 +706,6 @@ extension View {
     }
 }
 
-extension GraphicsContext3D {
-    func drawAxisMarkers(labels: Bool = true) {
-        // TODO: Really this should use line3d and clip to context.
-        for axis in Axis3D.allCases {
-            stroke(path: Path3D { path in
-                path.move(to: axis.vector * -5)
-                path.addLine(to: axis.vector * 5)
-            }, with: .color(axis.color))
-            if labels {
-                let negativeAxisLabel = Text("-\(axis)").font(.caption)
-                graphicsContext2D.draw(negativeAxisLabel, at: projection.worldSpaceToScreenSpace(axis.vector * -5))
-                let positiveAxisLabel = Text("+\(axis)").font(.caption)
-                graphicsContext2D.draw(positiveAxisLabel, at: projection.worldSpaceToScreenSpace(axis.vector * +5))
-            }
-        }
-    }
-}
-
 struct RasterizerOptionsView: View {
     @Binding
     var options: Rasterizer.Options
