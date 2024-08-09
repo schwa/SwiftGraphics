1fc7712783dab249ff6d0eca0799eaae3cff324e
Jonathan Wight <jwight@mac.com>
2024-08-09 15:12:07 -0700

    Clean up demos.

---
+++ Sources/Constraints3D/ZoomGestureViewModifier.swift
@@ -0,0 +1,45 @@
+import BaseSupport
+import SwiftUI
+
+public struct ZoomGestureViewModifier: ViewModifier {
+    @Binding
+    var zoom: Float
+
+    var range: ClosedRange<Float>
+
+    @State
+    var initialZoom: Float?
+
+
+    public init(zoom: Binding<Float>, range: ClosedRange<Float>) {
+        self._zoom = zoom
+        self.range = range
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .gesture(magnifyGesture)
+    }
+
+    func magnifyGesture() -> some Gesture {
+        MagnifyGesture()
+            .onEnded { _ in
+                initialZoom = nil
+            }
+            .onChanged { value in
+                if initialZoom == nil {
+                    initialZoom = zoom
+                }
+                guard let initialZoom else {
+                    fatalError("Cannot zoom without an initial zoom value.")
+                }
+                zoom = clamp(initialZoom / Float(value.magnification), to: range)
+            }
+    }
+}
+
+public extension View {
+    func zoomGesture(zoom: Binding<Float>, range: ClosedRange<Float> = -.infinity ... .infinity) -> some View {
+        modifier(ZoomGestureViewModifier(zoom: zoom, range: range))
+    }
+}
