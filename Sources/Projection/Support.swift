ab28b7741992093a888c51328e39a186958db1a0
Jonathan Wight <jwight@mac.com>
2024-08-11 08:24:36 -0700

    Reorg.

--- Sources/Projection/Support.swift
+++ Sources/Projection/Support.swift
@@ -2,18 +2,6 @@ import simd
 import SIMDSupport
 import SwiftUI
 
-//extension simd_float4x4 {
-//    var translation: SIMD3<Float> {
-//        columns.3.xyz
-//    }
-//}
-//
-//extension SIMD4 {
-//    var xyz: SIMD3<Scalar> {
-//        [x, y, z]
-//    }
-//}
-
 extension GraphicsContext.Shading {
     static func color(_ rgb: SIMD3<Float>) -> Self {
         .color(Color(red: Double(rgb.x), green: Double(rgb.y), blue: Double(rgb.z)))
