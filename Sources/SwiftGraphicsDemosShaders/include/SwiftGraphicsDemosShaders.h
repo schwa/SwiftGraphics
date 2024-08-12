63055d516a84481b68489a1bd3da2e675d406e05
Jonathan Wight <jwight@mac.com>
2024-08-11 21:03:08 -0700

    Move to legacy.

---
+++ Sources/SwiftGraphicsDemosShaders/include/SwiftGraphicsDemosShaders.h
@@ -0,0 +1,14 @@
+#import "CommonTypes.h"
+//#import "MetalSupport.h"
+//#import "ImmersiveShaders.h"
+//#import "PanoramicShaders.h"
+#import "VolumeShaders.h"
+//#import "GLSLCompat.h"
+//#import "Random.h"
+//
+#import "Common.h"
+//#import "SmoothPanoramaShaders.h"
+//#import "Support.h"
+
+//#import "PointCloudShader.h"
+//#import "GraphToy.h"
---
+++ Sources/SwiftGraphicsDemosShadersLegacy/include/Common.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __METAL_VERSION__
+#include <simd/simd.h>
+typedef simd_float2 float2;
+typedef simd_float3 float3;
+typedef simd_float4 float4;
+typedef simd_float3x3 float3x3;
+typedef simd_float4x4 float4x4;
+#endif
+
+#ifdef __METAL_VERSION__
+// TODO: Deprecate
+struct VertexIn {
+    float3 position  [[attribute(0)]];
+    float3 normal    [[attribute(1)]];
+    float2 texCoords [[attribute(2)]];
+};
+#else
+// TODO: Deprecate
+struct VertexIn {
+    float3 position;
+    float3 normal;
+    float2 texCoords;
+};
+#endif
