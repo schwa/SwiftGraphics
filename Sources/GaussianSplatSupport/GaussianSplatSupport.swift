137e2c1926b4ce183da73dfe30087a37681d0269
Jonathan Wight <schwa@schwa.io>
2024-09-24 13:03:37 -0700

    Cleanup

--- Sources/GaussianSplatSupport/GaussianSplatSupport.swift
+++ Sources/GaussianSplatSupport/GaussianSplatSupport.swift
@@ -1,20 +1,22 @@
 import BaseSupport
 import Foundation
 import Metal
+import os
+import RenderKitSceneGraph
 import simd
 import SIMDSupport
 
 // swiftlint:disable force_unwrapping
 
-public func max(lhs: PackedFloat3, rhs: PackedFloat3) -> PackedFloat3 {
+internal func max(lhs: PackedFloat3, rhs: PackedFloat3) -> PackedFloat3 {
     [max(lhs[0], rhs[0]), max(lhs[1], rhs[1]), max(lhs[2], rhs[2])]
 }
 
-public func min(lhs: PackedFloat3, rhs: PackedFloat3) -> PackedFloat3 {
+internal func min(lhs: PackedFloat3, rhs: PackedFloat3) -> PackedFloat3 {
     [min(lhs[0], rhs[0]), min(lhs[1], rhs[1]), min(lhs[2], rhs[2])]
 }
 
-public extension Collection where Element == PackedFloat3 {
+internal extension Collection where Element == PackedFloat3 {
     var bounds: (min: PackedFloat3, max: PackedFloat3) {
         (
             // swiftlint:disable:next reduce_into
@@ -26,6 +28,29 @@ public extension Collection where Element == PackedFloat3 {
 }
 
 // MARK: -
+
+internal extension Node {
+    func splats <Splat>(_ type: Splat.Type) -> SplatCloud<Splat>? where Splat: SplatProtocol {
+        content as? SplatCloud<Splat>
+    }
+}
+
+internal extension MTLRenderPassColorAttachmentDescriptor {
+    var size: SIMD2<Float> {
+        get throws {
+            guard let texture else {
+                throw BaseError.error(.invalidParameter)
+            }
+            return SIMD2<Float>(Float(texture.width), Float(texture.height))
+        }
+    }
+}
+
+internal func releaseAssert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = String(), file: StaticString = #file, line: UInt = #line) {
+    if !condition() {
+        fatalError(message(), file: file, line: line)
+    }
+}
 
 @dynamicMemberLookup
 public struct TupleBuffered<Element> {
