ee3645a8327a4fd41a167a9f7eebcd1b17b525d7
Jonathan Wight <jwight@mac.com>
2024-09-29 09:08:58 -0700

    Upstream merge.

--- Sources/SwiftGraphicsDemos/GaussianSplatDemos/GaussianSplatLobbyView.swift
+++ Sources/SwiftGraphicsDemos/GaussianSplatDemos/GaussianSplatLobbyView.swift
@@ -1,5 +1,6 @@
 import Constraints3D
 import GaussianSplatSupport
+import MetalKit
 import RenderKit
 import SwiftUI
 
@@ -20,7 +21,22 @@ public struct GaussianSplatLobbyView: View {
     private var progressiveLoad = true
 
     @State
-    private var backgroundColor = Color.black
+    private var backgroundColor = Color.white
+
+    @State
+    private var useSkyboxGradient = true
+
+    @State
+    private var skyboxGradient: LinearGradient = .init(
+        stops: [
+            .init(color: .white, location: 0),
+            .init(color: .white, location: 0.4),
+            .init(color: Color(red: 0.5294117647058824, green: 0.807843137254902, blue: 0.9215686274509803), location: 0.5),
+            .init(color: Color(red: 0.5294117647058824, green: 0.807843137254902, blue: 0.9215686274509803), location: 1)
+        ],
+        startPoint: .init(x: 0, y: 0),
+        endPoint: .init(x: 0, y: 1)
+    )
 
     @State
     private var source: SplatResource
@@ -68,6 +84,16 @@ public struct GaussianSplatLobbyView: View {
                         }
                         optionsView
                     }
+                    Button("Use Debug Colors") {
+                        backgroundColor = .init(red: 1, green: 1, blue: 1)
+                        skyboxGradient = .init(stops: [
+                            .init(color: .init(red: 1, green: 0, blue: 0).opacity(0), location: 1),
+                            .init(color: .init(red: 1, green: 0, blue: 0).opacity(1), location: 0)
+                            ],
+                            startPoint: .init(x: 0, y: 0),
+                            endPoint: .init(x: 0, y: 1)
+                        )
+                    }
                     Button("Go!") {
                         configuration.bounds = source.bounds
                         mode = .render
@@ -89,6 +115,9 @@ public struct GaussianSplatLobbyView: View {
                     let components = color.components!
                     configuration.clearColor = MTLClearColor(red: components[0], green: components[1], blue: components[2], alpha: components[3])
                 }
+                .onChange(of: skyboxGradient, initial: true) {
+                    updateSkyboxTexture()
+                }
                 #if os(macOS)
                 .frame(width: 320)
                 #endif
@@ -107,6 +136,33 @@ public struct GaussianSplatLobbyView: View {
             }
         }
     }
+
+    func updateSkyboxTexture() {
+        if useSkyboxGradient {
+            let image = skyboxGradient.image(size: .init(width: 1024, height: 1024))
+
+            guard var cgImage = ImageRenderer(content: image).cgImage else {
+                fatalError()
+            }
+            let bitmapInfo: CGBitmapInfo
+            if cgImage.byteOrderInfo == .order32Little {
+                bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Big.rawValue)
+            } else {
+                bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue)
+            }
+            cgImage = cgImage.convert(bitmapInfo: bitmapInfo)!
+            let textureLoader = MTKTextureLoader(device: device)
+            let texture = try! textureLoader.newTexture(cgImage: cgImage, options: nil)
+            texture.label = "Skybox Gradient"
+            configuration.skyboxTexture = texture
+            print("Set skyboxTexture to \(texture)")
+        }
+        else {
+            configuration.skyboxTexture = nil
+        }
+
+    }
+
 
     @ViewBuilder
     var optionsView: some View {
@@ -154,6 +210,15 @@ public struct GaussianSplatLobbyView: View {
                     Text("Colour of background (behind the splats)").font(.caption)
                 }
             }
+            LabeledContent("Skybox Gradient") {
+                VStack(alignment: .leading) {
+                    Toggle("Skybox Gradient", isOn: $useSkyboxGradient)
+                    if useSkyboxGradient {
+                        LinearGradientEditor(value: $skyboxGradient)
+                    }
+                    Text("Use a gradient skybox (above the background color, behind the splats)").font(.caption)
+                }
+            }
             LabeledContent("Progressive Load") {
                 VStack(alignment: .leading) {
                     Toggle("Progressive Load", isOn: $progressiveLoad)
@@ -170,4 +235,53 @@ public struct GaussianSplatLobbyView: View {
             }
         }
     }
+}
+
+extension CGImage {
+    func convert(bitmapInfo: CGBitmapInfo) -> CGImage? {
+        let width = width
+        let height = height
+        let bitsPerComponent = 8
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue) else {
+            return nil
+        }
+        context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
+        return context.makeImage()
+    }
+}
+
+func convertCGImageEndianness2(_ inputImage: CGImage) -> CGImage? {
+    let width = inputImage.width
+    let height = inputImage.height
+    let bitsPerComponent = 8
+    let bytesPerPixel = 4
+    let bytesPerRow = width * bytesPerPixel
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+
+    // Choose the appropriate bitmap info for the target endianness
+    let bitmapInfo: CGBitmapInfo
+    if inputImage.byteOrderInfo == .order32Little {
+        bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Big.rawValue)
+    } else {
+        bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue)
+    }
+
+    guard let context = CGContext(data: nil,
+                                  width: width,
+                                  height: height,
+                                  bitsPerComponent: bitsPerComponent,
+                                  bytesPerRow: bytesPerRow,
+                                  space: colorSpace,
+                                  bitmapInfo: bitmapInfo.rawValue) else {
+        return nil
+    }
+
+    // Draw the original image into the new context
+    context.draw(inputImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+    // Create a new CGImage from the context
+    return context.makeImage()
 }
