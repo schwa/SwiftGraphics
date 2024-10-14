cdd6dc8e4470616bd3ea734143137297754dd63a
Jonathan Wight <jwight@mac.com>
2024-10-14 16:41:31 -0700

    Swiftlint

--- Sources/GaussianSplatSupport/SplatConverter.swift
+++ Sources/GaussianSplatSupport/SplatConverter.swift
@@ -5,15 +5,9 @@ import SIMDSupport
 public extension SplatC {
     init(_ other: SplatB) {
         let position = PackedHalf3(SIMD3<Float>(other.position))
-
-        // TODO: SRGB to Linear
-
-        //        let color = other.color.xyz.sRGBToLinear()
         let rgb = (SIMD3<Float>(other.color.xyz) / 255).sRGBToLinear()
         let alpha = Float(other.color[3]) / 255
-
         let color = PackedHalf4(SIMD4<Float>(rgb, alpha))
-
         let rotation = simd_quatf(vector: SIMD4(x: Float(other.rotation[1]) - 128,
                                                 y: Float(other.rotation[2]) - 128,
                                                 z: Float(other.rotation[3]) - 128,
@@ -22,7 +16,6 @@ public extension SplatC {
         let cov3D = transform * transform.transpose
         let cov_a = PackedHalf3(x: Float16(cov3D[0, 0]), y: Float16(cov3D[0, 1]), z: Float16(cov3D[0, 2]))
         let cov_b = PackedHalf3(x: Float16(cov3D[1, 1]), y: Float16(cov3D[1, 2]), z: Float16(cov3D[2, 2]))
-
         self = SplatC(position: position, color: color, cov_a: cov_a, cov_b: cov_b)
     }
 }
