e6ef381d92d62bb2712b1427c12367457d577238
Jonathan Wight <jwight@mac.com>
2024-12-09 09:38:40 -0800

    Add debug.

--- Sources/Constraints3D/BallController.swift
+++ Sources/Constraints3D/BallController.swift
@@ -14,12 +14,15 @@ public struct NewBallControllerViewModifier: ViewModifier {
     @Binding
     var transform: Transform
 
+    var debug: Bool = false
+
     var pitchRange: ClosedRange<Angle> = .degrees(-90) ... .degrees(90)
     var yawRange: ClosedRange<Angle> = .degrees(0) ... .degrees(360)
 
-    public init(constraint: NewBallConstraint, transform: Binding<Transform>) {
+    public init(constraint: NewBallConstraint, transform: Binding<Transform>, debug: Bool = false) {
         self.constraint = constraint
         self._transform = transform
+        self.debug = debug
         // TODO: compute pitch yaw from transform
     }
 
@@ -29,9 +32,15 @@ public struct NewBallControllerViewModifier: ViewModifier {
             .draggableParameter($yaw.degrees, axis: .horizontal, range: yawRange.degrees, scale: 0.1, behavior: .wrapping)
             .onChange(of: [pitch, yaw], initial: true) {
                 transform = constraint.transform(for: RollPitchYaw(pitch: pitch, yaw: yaw))
+                if debug {
+                    print("pitch: \(pitch.degrees), yaw: \(yaw.degrees)")
+                }
             }
             .onChange(of: constraint, initial: true) {
                 transform = constraint.transform(for: RollPitchYaw(pitch: pitch, yaw: yaw))
+                if debug {
+                    print("pitch: \(pitch.degrees), yaw: \(yaw.degrees)")
+                }
             }
     }
 }
