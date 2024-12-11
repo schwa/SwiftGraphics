1fe8236675923cee3a548fd85db106f66c64e1a8
Jonathan Wight <jwight@mac.com>
2024-12-11 11:28:06 -0800

    Make ball controller work on matrices directly.

--- Sources/Constraints3D/BallController.swift
+++ Sources/Constraints3D/BallController.swift
@@ -12,14 +12,14 @@ public struct NewBallControllerViewModifier: ViewModifier {
     private var yaw: Angle = .zero
 
     @Binding
-    var transform: Transform
+    var transform: simd_float4x4
 
     var debug: Bool = false
 
     var pitchRange: ClosedRange<Angle> = .degrees(-90) ... .degrees(90)
     var yawRange: ClosedRange<Angle> = .degrees(0) ... .degrees(360)
 
-    public init(constraint: NewBallConstraint, transform: Binding<Transform>, debug: Bool = false) {
+    public init(constraint: NewBallConstraint, transform: Binding<simd_float4x4>, debug: Bool = false) {
         self.constraint = constraint
         self._transform = transform
         self.debug = debug
@@ -31,17 +31,29 @@ public struct NewBallControllerViewModifier: ViewModifier {
             .draggableParameter($pitch.degrees, axis: .vertical, range: pitchRange.degrees, scale: 0.1, behavior: .clamping)
             .draggableParameter($yaw.degrees, axis: .horizontal, range: yawRange.degrees, scale: 0.1, behavior: .wrapping)
             .onChange(of: [pitch, yaw], initial: true) {
-                transform = constraint.transform(for: RollPitchYaw(pitch: pitch, yaw: yaw))
+                transform = constraint.transform(for: RollPitchYaw(pitch: pitch, yaw: yaw)).matrix
                 if debug {
-                    print("pitch: \(pitch.degrees), yaw: \(yaw.degrees)")
+                    print("Pitch/Yaw changed. pitch: \(pitch.degrees), yaw: \(yaw.degrees), transform: \(transform)")
                 }
             }
-            .onChange(of: constraint, initial: true) {
-                transform = constraint.transform(for: RollPitchYaw(pitch: pitch, yaw: yaw))
+            .onChange(of: constraint) {
+                transform = constraint.transform(for: RollPitchYaw(pitch: pitch, yaw: yaw)).matrix
                 if debug {
-                    print("pitch: \(pitch.degrees), yaw: \(yaw.degrees)")
+                    print("Constraint changed. pitch: \(pitch.degrees), yaw: \(yaw.degrees)")
                 }
             }
+            .onTapGesture {
+                if debug {
+                    pitch = .zero
+                    yaw = .zero
+                }
+            }
+    }
+}
+
+public extension NewBallControllerViewModifier {
+    init(constraint: NewBallConstraint, transform: Binding<Transform>, debug: Bool = false) {
+        self.init(constraint: constraint, transform: Binding(get: { transform.wrappedValue.matrix }, set: { transform.wrappedValue = Transform($0) }), debug: debug)
     }
 }
 
@@ -56,7 +68,7 @@ public struct NewBallConstraint: Equatable {
         self.radius = radius
     }
 
-    func transform(for rotation: RollPitchYaw) -> Transform {
+    public func transform(for rotation: RollPitchYaw) -> Transform {
         Transform(simd_float4x4(translate: [0, 0, radius]) * rotation.toMatrix4x4(order: .rollPitchYaw))
     }
 }
