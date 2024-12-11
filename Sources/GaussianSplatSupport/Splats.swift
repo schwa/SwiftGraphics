1250848dfca26e0d212b1e37f761287f4ff0f368
Jonathan Wight <jwight@mac.com>
2024-12-11 10:03:21 -0800

    Move SplatD & friends.

--- Sources/GaussianSplatSupport/Splats.swift
+++ Sources/GaussianSplatSupport/Splats.swift
@@ -43,3 +43,43 @@ extension SplatC: SplatProtocol {
         .init(position)
     }
 }
+
+public struct SplatD: Equatable {
+    public var position: PackedFloat3
+    public var scale: PackedFloat3
+    public var color: SIMD4<Float>
+    public var rotation: Rotation
+
+    public init(position: PackedFloat3, scale: PackedFloat3, color: SIMD4<Float>, rotation: Rotation) {
+        self.position = position
+        self.scale = scale
+        self.color = color
+        self.rotation = rotation
+    }
+}
+
+public extension SplatB {
+    init(_ other: SplatD) {
+        let color = SIMD4<UInt8>(other.color * 255)
+        let rotation_vector = other.rotation.quaternion.vectorRealFirst
+        let rotation = ((rotation_vector / rotation_vector.length) * 128 + 128).clamped(to: 0...255)
+        self = SplatB(position: other.position, scale: other.scale, color: color, rotation: SIMD4<UInt8>(rotation))
+    }
+}
+
+public extension SplatC {
+    init(_ other: SplatD) {
+        let transform = simd_float3x3(other.rotation.quaternion) * simd_float3x3(diagonal: SIMD3<Float>(other.scale))
+        let cov3D = transform * transform.transpose
+        let cov_a = PackedHalf3(x: Float16(cov3D[0, 0]), y: Float16(cov3D[0, 1]), z: Float16(cov3D[0, 2]))
+        let cov_b = PackedHalf3(x: Float16(cov3D[1, 1]), y: Float16(cov3D[1, 2]), z: Float16(cov3D[2, 2]))
+
+        self = SplatC(position: PackedHalf3(SIMD3<Float>(other.position)), color: PackedHalf4(other.color), cov_a: cov_a, cov_b: cov_b)
+    }
+}
+
+extension simd_quatf {
+    var vectorRealFirst: simd_float4 {
+        [vector.w, vector.x, vector.y, vector.z]
+    }
+}
